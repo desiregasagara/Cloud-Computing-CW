868a8b8f6d75268fc610cd55dc9a8f9270819c13
desiregasagara <56978156+desiregasagara@users.noreply.github.com>
2020-11-29 23:21:16 +0000

    Add files via upload

--- Anagram.java
+++ Anagram.java
@@ -1,73 +1,125 @@
 import java.io.IOException;
+
 import java.util.Arrays;
+
 import java.util.Collection;
+
 import java.util.HashSet;
+
 import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
+
 import org.apache.hadoop.fs.Path;
+
 import org.apache.hadoop.io.IntWritable;
+
 import org.apache.hadoop.io.Text;
+
 import org.apache.hadoop.mapreduce.Job;
+
 import org.apache.hadoop.mapreduce.Mapper;
+
 import org.apache.hadoop.mapreduce.Reducer;
+
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Anagram {
+    static Collection<Text> anagrams = new HashSet<Text>();
 
-        static Collection<Text> anagrams = new HashSet<Text>();
+    public static class AnagramMakerMapper extends
 
-        public static class AnagramMakerMapper
-                extends Mapper<Object, Text, Text, Text> {
+            Mapper<Object, Text, Text, Text> {
 
-            public void map(Object key, Text value, Context context
-            ) throws IOException, InterruptedException {
-                StringTokenizer itr = new StringTokenizer(value.toString());
+        public void map(Object key, Text value, Context context)
 
-                while (itr.hasMoreTokens()) {
-                    String word = itr.nextToken().replace(',', ' ');
-                    char[] arr = word.toCharArray();
-                    Arrays.sort(arr);
-                    String wordKey = new String(arr);
-                    context.write(new Text(wordKey), new Text(word));
-                }
-            }
-        }
+                throws IOException, InterruptedException {
 
-        public static class AnagramAggregatorReducer
-                extends Reducer<Text, Text, Text, Text> {
-            public void reduce(Text key, Iterable<Text> values,
-                               Context context
-            ) throws IOException, InterruptedException {
-                // Collection<Text> anagrams = new HashSet<Text>();
-                Text anagramlist=new Text();
-                String anagram = "";
+            StringTokenizer itr = new StringTokenizer(value.toString());
 
-                for (Text val : values) {
-                        anagram = anagram + '~' + val.toString();
-                    }
-                StringTokenizer newtoken=new StringTokenizer(anagram,"~");
-                if(newtoken.countTokens()>=2) {
-                    anagram = anagram.replace("~", ",");
-                    anagramlist.set(anagram);
-                    context.write(key,anagramlist);
-                }
+            while (itr.hasMoreTokens()) {
 
-                // anagrams.add(val);
-                }
+                String word = itr.nextToken();
+
+                char[] arr = word.toCharArray();
+
+                Arrays.sort(arr);
+
+                String wordKey = new String(arr);
+
+                context.write(new Text(wordKey), new Text(word));
+
             }
 
-        public static void main(String[] args) throws Exception {
-            Configuration conf = new Configuration();
-            Job job = Job.getInstance(conf, "anagram");
-            job.setJarByClass(Anagram.class);
-            job.setMapperClass(AnagramMakerMapper.class);
-            job.setReducerClass(AnagramAggregatorReducer.class);
-            job.setOutputKeyClass(Text.class);
-            job.setOutputValueClass(Text.class);
-            FileInputFormat.addInputPath(job, new Path(args[0]));
-            FileOutputFormat.setOutputPath(job, new Path(args[1]));
-            System.exit(job.waitForCompletion(true) ? 0 : 1);
         }
 
     }
+
+    // Does not summarize, but simply collets the list of anagramming words for
+
+    // a key
+
+    public static class AnagramAggregatorReducer extends
+
+            Reducer<Text, Text, Text, Text> {
+
+        public void reduce(Text key, Iterable<Text> values, Context context)
+
+                throws IOException, InterruptedException {
+
+            // Collection<Text> anagrams = new HashSet<Text>();
+
+            String anagram = null;
+
+            for (Text val : values) {
+
+                if (anagram == null) {
+
+                    anagram = val.toString();
+
+
+
+                } else {
+
+                    anagram = anagram + ',' + val.toString();
+
+                }
+
+                // anagrams.add(val);
+
+            }
+
+            context.write(key, new Text(anagram));
+
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf, "word count");
+
+        job.setJarByClass(Anagram.class);
+
+        job.setMapperClass(AnagramMakerMapper.class);
+
+        job.setReducerClass(AnagramAggregatorReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+    }
+
+}
+
 
