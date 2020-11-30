782e4c17f6178e70e217c471460f1d86d9badb5d
desiregasagara <56978156+desiregasagara@users.noreply.github.com>
2020-11-30 19:50:31 +0000

    Add files via upload

--- Anagram.java
+++ Anagram.java
@@ -1,125 +1,80 @@
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
-
 import java.util.Arrays;
-
 import java.util.Collection;
-
 import java.util.HashSet;
-
 import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.fs.Path;
-
 import org.apache.hadoop.io.IntWritable;
-
 import org.apache.hadoop.io.Text;
-
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Job;
-
 import org.apache.hadoop.mapreduce.Mapper;
-
 import org.apache.hadoop.mapreduce.Reducer;
-
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Anagram {
-    static Collection<Text> anagrams = new HashSet<Text>();
 
-    public static class AnagramMakerMapper extends
+    static Collection<Text> anagramlist = new HashSet<Text>();
 
-            Mapper<Object, Text, Text, Text> {
 
-        public void map(Object key, Text value, Context context)
 
-                throws IOException, InterruptedException {
+    public static class AnagramMakerMapper
+            extends Mapper<Object, Text, Text, Text> {
 
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
             StringTokenizer itr = new StringTokenizer(value.toString());
 
             while (itr.hasMoreTokens()) {
-
-                String word = itr.nextToken();
-
+                String word = itr.nextToken().replace(',', ' ');
                 char[] arr = word.toCharArray();
-
                 Arrays.sort(arr);
-
                 String wordKey = new String(arr);
-
                 context.write(new Text(wordKey), new Text(word));
-
             }
-
         }
-
     }
 
-    // Does not summarize, but simply collets the list of anagramming words for
-
-    // a key
-
-    public static class AnagramAggregatorReducer extends
-
-            Reducer<Text, Text, Text, Text> {
-
-        public void reduce(Text key, Iterable<Text> values, Context context)
-
-                throws IOException, InterruptedException {
-
+    public static class AnagramAggregatorReducer
+            extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values,
+                           Context context
+        ) throws IOException, InterruptedException {
             // Collection<Text> anagrams = new HashSet<Text>();
-
-            String anagram = null;
+            Text anagramlist=new Text();
+            StringBuilder anagram = new StringBuilder();
 
             for (Text val : values) {
-
-                if (anagram == null) {
-
-                    anagram = val.toString();
-
-
-
-                } else {
-
-                    anagram = anagram + ',' + val.toString();
-
-                }
-
-                // anagrams.add(val);
-
+                anagram.append(val.toString()).append(",");
+            }
+            StringTokenizer newtoken=new StringTokenizer(anagram.toString(),",");
+            if(newtoken.countTokens()>=2) {
+                anagramlist.set(anagram.substring(0,anagram.length()-1));
+                context.write(key,anagramlist);
             }
 
-            context.write(key, new Text(anagram));
-
+            // anagrams.add(val);
         }
-
     }
 
     public static void main(String[] args) throws Exception {
-
         Configuration conf = new Configuration();
-
-        Job job = Job.getInstance(conf, "word count");
-
+        Job job = Job.getInstance(conf, "anagram");
         job.setJarByClass(Anagram.class);
-
         job.setMapperClass(AnagramMakerMapper.class);
-
         job.setReducerClass(AnagramAggregatorReducer.class);
-
         job.setOutputKeyClass(Text.class);
-
         job.setOutputValueClass(Text.class);
-
         FileInputFormat.addInputPath(job, new Path(args[0]));
-
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-
     }
 
 }
+
 
 
