31541fc18c02fc03982a660bbe958926aef08e14
desiregasagara <56978156+desiregasagara@users.noreply.github.com>
2020-12-03 15:02:46 +0000

    Add files via upload

--- Anagram.java
+++ Anagram.java
@@ -25,7 +25,6 @@ public class Anagram {
             ArrayList<String> stopwords =new ArrayList<String>(Arrays.asList("'tis","'twas","a","able","about","across","after","ain't","all","almost","also","am","among","an","and","any","are","aren't","as","at","be","because","been","but","by","can","can't","cannot","could","could've","couldn't","dear","did","didn't","do","does","doesn't","don't","either","else","ever","every","for","from","get","got","had","has","hasn't","have","he","he'd","he'll","he's","her","hers","him","his","how","how'd","how'll","how's","however","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","just","least","let","like","likely","may","me","might","might've","mightn't","most","must","must've","mustn't","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","shan't","she","she'd","she'll","she's","should","should've","shouldn't","since","so","some","than","that","that'll","that's","the","their","them","then","there","there's","these","they","they'd","they'll","they're","they've","this","tis","to","too","twas","us","wants","was","wasn't","we","we'd","we'll","we're","were","weren't","what","what'd","what's","when","when","when'd","when'll","when's","where","where'd","where'll","where's","which","while","who","who'd","who'll","who's","whom","why","why'd","why'll","why's","will","with","won't","would","would've","wouldn't","yet","you","you'd","you'll","you're","you've","your"));
             ArrayList<String> listOfWords = new ArrayList<String>();
             Text wText =new Text();
-            Text vText =new Text();
             StringTokenizer itr = new StringTokenizer(value.toString());
 
             while (itr.hasMoreTokens()) {
@@ -39,12 +38,10 @@ public class Anagram {
                     char[] arr = newword.toCharArray();
                     Arrays.sort(arr);
                     String wordKey = new String(arr);
-                    wText.set(wordKey);
-                    vText.set(newword);
-                    context.write(wText,vText);
+                    context.write(new Text(wordKey), new Text(newword));
                 }
     }
-    }
+        }
 
     public static class Combiner extends org.apache.hadoop.mapreduce.Reducer<Text, Text, Text, Text> {
         protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
